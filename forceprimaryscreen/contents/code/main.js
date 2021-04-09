2a05e756ea8ae9c56cd42b8bac1c6a796d9f1294
Ronoaldo JLP <ronoaldo@gmail.com>
2021-04-09 11:18:19 -0300

    Fixed a bug when the desktop was unusable.

--- forceprimaryscreen/contents/code/main.js
+++ forceprimaryscreen/contents/code/main.js
@@ -2,12 +2,18 @@
  * Kwin script to place windows always on your primary screen.
  *
  */
+
+function shouldMove(client) {
+	return !client.specialWindow && !client.modal;
+}
+
 workspace.clientAdded.connect(function(client) {
 	// If we have more screens, force new window to be on primary
-	if (workspace.numScreens > 1) {
+	if (workspace.numScreens > 1 && shouldMove(client)) {
 		var primaryScreenGeometry = workspace.clientArea(KWin.ScreenArea, 0, 0);
-		print("Multiple screens setup; moving", client.windowId, "to primary screen at",
-		primaryScreenGeometry.x, primaryScreenGeometry.y);
+		print("Multiple screens setup; moving",
+			client.caption, " (", client.windowId, ") ",
+			"to primary screen at",	primaryScreenGeometry.x, primaryScreenGeometry.y);
 		// Move client to primary screen geometry (x,y) but keep original window size
 		var g = client.geometry;
 		g.x = primaryScreenGeometry.x;
